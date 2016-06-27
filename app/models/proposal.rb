89d4ff8ed1c2702903880ce23dc44d29f844e96a
softwarenacho <trsgarp@gmail.com>
2016-06-27 18:45:08 -0500

    Added counter method

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -8,4 +8,12 @@ class Proposal < ActiveRecord::Base
                 "proposal[twitter_handle]" => self.twitter_handle}        
       Net::HTTP.post_form(URI.parse(url), params)  
   end
+
+  def counter
+      api_token = Rails.application.secrets.codea_tag_api_token            
+      url = "http://codeatag.herokuapp.com/proposal_counter"
+      params = {"api_token" => api_token, 
+                "proposal[twitter_handle]" => self.twitter_handle}        
+      Net::HTTP.post_form(URI.parse(url), params)  
+  end
 end
